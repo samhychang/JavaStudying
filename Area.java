56848f6f4a509b04fa4cb43340aa18108c25c9dd
samhychang <samhychang@gmail.com>
2018-07-03 15:09:27 +0800

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,14 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 	final double PI = 3.1416;  //pi，近似值
+	 	double r, area;
+	 	r = 10.8; //圓的半徑
+	 	area = PI * r * r; //計算面積
+	 	System.out.println("Area of circle is: " + area);
+
+	}
+
+}
